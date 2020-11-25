bee99c39258c54821636da62a9e7d23cf879b95a
lloydnoone <lloydnoone92@gmail.com>
2020-11-25 17:44:40 +0000

    add fetch/save to user class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import axios from 'axios'
+import { User } from './models/User'
 
-axios.post('http://localhost:3000/users', {
-  name: "myname",
-  age: 20
-})
+const user = new User({ name: 'new record', age: 0 })
+
+user.save()
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,9 @@
+import axios, { AxiosResponse } from 'axios'
+
 interface UserProps {
   name?: string;
   age?: number;
+  id?: number;
 }
 
 type Callback = () => void
@@ -33,6 +36,24 @@ export class User {
     handlers.forEach(callback => {
       callback()
     })
+  }
+
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data)
+      })
+  }
+
+  save(): void {
+    const id = this.get('id')
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data)
+    } else {
+      axios.post('http://localhost:3000/users/', this.data)
+    }
+    
   }
 }
 
