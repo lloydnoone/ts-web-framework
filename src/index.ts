0333c33659955a23355b6fbc8ac643f3ba8523fe
lloydnoone <lloydnoone92@gmail.com>
2020-12-04 14:48:32 +0000

    add CollectionViews

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import { UserForm } from './views/UserForm'
-import { User } from './models/User'
+import { UserList } from './views/UserList'
+import { Collection } from './models/Collection'
+import { User, UserProps } from './models/User'
 
-const user = User.buildUser({ name: 'NAME', age: 20 })
+const users = new Collection(
+  'http://localhost:3000/users', 
+  (json: UserProps) => {
+    return User.buildUser(json)
+  })
 
-const root = document.getElementById('root')
+  users.on('change', () => {
+    const root = document.getElementById('root')
+    if (root) {
+      new UserList(root,users).render()
+    }
+  })
 
-if (root) {
-  const userForm = new UserForm(root, user)
-  userForm.render()
-} else {
-  throw new Error('Root element not found')
-}
+  users.fetch()
